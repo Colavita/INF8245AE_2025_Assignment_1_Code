a914bbe9e283ce9e1e9244b04a83f91005547b9a
Colavita <m.colavita@bell.net>
2025-09-23 13:25:23 -0400

    Implement gradient descent with varying learning rates and plot training loss

--- q3_1.py
+++ q3_1.py
@@ -7,32 +7,43 @@ def ridge_gradient(X: np.ndarray, y: np.ndarray, w: np.ndarray, lamb: float) -> 
     Computes the gradient of Ridge regression loss.
     ∇L(w) = -2/n X^T (y - X w) + 2 λ w
     """
-    # WRITE YOUR CODE HERE...
+    n = X.shape[0]
+    grad = (-2/n) * (X.T @ (y - X @ w)) + 2 * lamb * w
+    return grad
 
 
 # Part (b)
 def learning_rate_exp_decay(eta0: float, t: int, k_decay: float) -> float:
-    # WRITE YOUR CODE HERE...
-
+    return eta0 * np.exp(-k_decay * t)
 
 
 # Part (c)
 def learning_rate_cosine_annealing(eta0: float, t: int, T: int) -> float:
-    # WRITE YOUR CODE HERE...
+    return eta0 * 0.5 * (1 + np.cos((np.pi * t) / T))
 
 
 # Part (d)
 def gradient_step(X: np.ndarray, y: np.ndarray, w: np.ndarray, lamb:float, eta: float) -> np.ndarray:
-    # WRITE YOUR CODE HERE...
-
+    grad = ridge_gradient(X, y, w, lamb=lamb)
+    return w - eta * grad
+    
 
 # Part (e)
 def gradient_descent_ridge(X, y, lamb=1.0, eta0=0.1, T=500, schedule="constant", k_decay=0.01):
-    # WRITE YOUR CODE HERE...
+    n = len(X)
+    d = X.shape[1]
+    w = np.zeros(d)
+    L = np.zeros(T)
+    for t in range(T):
+        if schedule == "constant":
+            eta = eta0
+        elif schedule == "exp_decay":
+            eta = learning_rate_exp_decay(eta0, t, k_decay)
+        elif schedule == "cosine":
+            eta = learning_rate_cosine_annealing(eta0, t, T)
+        
+        w = gradient_step(X, y, w, lamb=lamb, eta=eta)
+        y_hat = X @ w
+        L[t] = (1/n) * np.sum((y - y_hat)**2) + lamb * np.sum(w**2)
 
-
-# Remove the following line if you are not using it:
-if __name__ == "__main__":
-
-    # If you want to test your functions, write your code here.
-    # If you write it outside this snippet, the autograder will fail!
+    return w, L
--- q3_2.py
+++ q3_2.py
@@ -1,7 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from q3_1 import gradient_descent_ridge
-from q1_1 import rmse
+from q1_1 import data_matrix_bias, predict, rmse
 
-# Write your code here ...
-# Not autograded — function names and structure are flexible.
+X_train = pd.read_csv("X_train.csv").values
+X_test = pd.read_csv("X_test.csv").values
+y_train = pd.read_csv("y_train.csv").values.flatten()
+y_test = pd.read_csv("y_test.csv").values.flatten()
+
+X_train = data_matrix_bias(X_train)
+X_test = data_matrix_bias(X_test)
+
+schedules = ["constant", "exp_decay", "cosine"]
+eta0 = 0.001
+k = 0.001
+T= 1000
+lamb = 1.0
+
+for schedule in schedules:
+    w, L = gradient_descent_ridge(X_train, y_train, lamb=lamb, eta0=eta0, T=T, schedule=schedule, k_decay=k)
+
+    plt.plot(range(T), L, label= schedule)
+    
+    y_hat = predict(X_test, w)
+    print(f"{schedule} RMSE:", rmse(y_test, y_hat))
+    
+
+plt.xlabel("Iteration")
+plt.ylabel("Training Loss")
+plt.title("Training Loss vs Iterations")
+plt.legend()
+plt.show()
